d0b76474757dd17076d82924f5b88e61e6a6398a
chenzhongyang <chen_980919.@163.com>
2018-01-18 21:42:43 +0800

    登录完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
-    <!-- 登录 -->
-    <div>登录</div>
-    <ul>
-        <li>
-            <label for="uname">用户名:</label>
-            <input type="text" v-model="username" id="uname">
-        </li>
-        <li>
-            <label for="pw">密码:</label>
-            <input type="text" v-model="password" id="pw">
-        </li>
-        <li>
-            <input type="button" value="登陆" @click="login">
-        </li>
-    </ul>
+  <div class="info">
+    <div class="avatar">
+      <img src="../assets/logo.jpg">
+    </div>
+    <el-form ref="loginForm" :model="form" :rules="rules" class='container'>
+    <div class="userInfo">
+        <el-form-item prop='username'>
+      <el-input type="text" prefix-icon="myicon myicon-user" v-model="form.username"  placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item prop='password'>
+      <el-input type="password" prefix-icon="myicon myicon-key" v-model="form.password" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="login">登录</el-button>
+      <el-button @click="empty">重置</el-button>
+    </el-form-item>
+    </div>
+</el-form>
   </div>
 </template>
 
@@ -23,28 +25,95 @@ import {login} from '../api/api.js'
 export default {
   data () {
     return {
-      username: '',
-      password: ''
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
     login: function () {
-      let params = {
-        username: this.username,
-        password: this.password
-      }
-      login(params).then(res => {
-        if (res.meta.status === 200) {
-          this.$router.push({name: 'home'})
-        } else {
-          console.log('用户名或密码错误')
+      this.$refs['loginForm'].validate(valid => {
+        if (valid) {
+          let params = {
+            username: this.form.username,
+            password: this.form.password
+          }
+          login(params).then(res => {
+            if (res.meta.status === 200) {
+              // 存在token的值
+              localStorage.setItem('mytoken', res.data.token)
+              // 跳转到主页面
+              this.$router.push({name: 'home'})
+            } else {
+              // 登陆失败
+              this.$notify.error({
+                title: '错误',
+                message: res.meta.msg
+              })
+            }
+          })
         }
       })
+    },
+    empty: function () {
+      this.$refs['loginForm'].resetFields()
     }
   }
 }
 </script>
 
 <style scoped>
-
+.info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    box-sizing: border-box;
+    background-color: #2f4050;
+  }
+  .container {
+    box-sizing: border-box;
+    margin: 180px auto 0;
+    width: 500px;
+    height: 340px;
+    background-color: #fff;
+    position: relative;
+  }
+  .avatar {
+    position: absolute;
+    left: 50%;
+    top: 110px;
+    width: 120px;
+    height: 120px;
+    border: 10px solid #FFF;
+    margin-left: -70px;
+    border-radius: 50%;
+    box-shadow: 0 1px 5px #CCC;
+    background-color: pink;
+    overflow: hidden;
+    z-index: 9999;
+  }
+  .avatar img {
+    width: 120px;
+    height: 120px;
+  }
+  .userInfo {
+    position: absolute;
+    text-align: center;
+    width: 400px;
+    margin: 100px 50px;
+  }
+  .btn {
+    width: 400px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,28 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 加载UI组件
+import ElementUI from 'element-ui'
+// 加载主题
+import 'element-ui/lib/theme-chalk/index.css'
+// 启用ElementUI
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  let user = localStorage.getItem('mytoken')
+  console.log(user)
+  if (user) {
+    next()
+  } else {
+    if (to.path !== '/') {
+      next({path: '/'})
+    } else {
+      next()
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
