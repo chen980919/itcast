533e060e6ea3a685cb05573304801579fe093d59
chenzhongyang <chen_980919.@163.com>
2018-01-20 14:49:41 +0800

    用户管理布局完成

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
-   用户管理页面
+   <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <div>
+      <el-input placeholder="请输入内容" class="search">
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+      <el-button type="success" plain>添加用户</el-button>
+    </div>
+    <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
+  <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400">
+    </el-pagination>
   </div>
 </template>
 
@@ -8,12 +46,44 @@
 export default {
   data () {
     return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
   },
   methods: {
+    // handleSizeChange(val) {
+    //   console.log(`每页 ${val} 条`)
+    // },
+    // handleCurrentChange(val) {
+    //   console.log(`当前页: ${val}`)
+    // }
   }
 }
 </script>
 
 <style scoped>
+.el-breadcrumb {
+  background-color: #D3DCE6;
+  height: 50px;
+  line-height: 50px;
+  padding-left: 15px;
+}
+.search {
+    width: 300px;
+  }
 </style>
